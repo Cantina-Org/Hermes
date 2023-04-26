e117f971a39714554492194c4211242c748be222
matyu <degueurce.mathieu@gmail.com>
2023-04-26 16:50:07 +0200

    - Affichage des utilisateur

--- client/js/private-message.js
+++ client/js/private-message.js
@@ -10,6 +10,41 @@ function getCookie(name){
    return false
 }
 
+function addUserToList(
+    id,
+    name = 'NO NAME',
+    color = '#000',
+    onClick
+) {
+   const userList = document.querySelector('.user-list');
+
+   const cellRow = document.createElement('li');
+   cellRow.setAttribute('id', id);
+   cellRow.setAttribute('class', 'cell-row');
+
+   const cellImage = document.createElement('div');
+   cellImage.setAttribute('class', 'cell-image');
+   cellImage.setAttribute('style', `background-color: ${color}`);
+   cellRow.appendChild(cellImage);
+
+   const cellRowText = document.createElement('div');
+   cellRowText.setAttribute('class', 'cell-row-text');
+   cellRow.appendChild(cellRowText);
+
+   const cellName = document.createElement('p');
+   cellName.setAttribute('class', 'cell-name');
+   cellName.innerText = name;
+   cellRowText.appendChild(cellName);
+
+   userList.appendChild(cellRow);
+
+   cellRow.addEventListener('click', () => {
+      for (let c of userList.children) c.setAttribute('selected', false);
+      cellRow.setAttribute('selected', true);
+      onClick(cellRow.attributes.id.nodeValue);
+   });
+}
+
 
 socket.on('connect', () => {
    socket.emit('login', {userToken: getCookie('token'), privateMessage: true});
@@ -21,5 +56,7 @@ socket.on('user-list', (data) => {
    for (const i of data.userList){
       console.log('userToken: ' + i.token);
       console.log('userName: ' + i.user_name);
+      if (i.token === getCookie('token')) continue
+      addUserToList(i.token, i.user_name, '#da04f1', () => {console.log(i.token)});
    }
 });
