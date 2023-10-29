98c49c0fd6357efdf76f838f159491bce7bfea39
mathieu <degueurce.mathieu@gmail.com>
2023-10-29 09:42:00 +0100

    Selection de l'utilisateur

--- client/js/debug_choose_user.js
+++ client/js/debug_choose_user.js
@@ -3,5 +3,12 @@ let socket = io();
 socket.emit('debug_choose_user');
 
 socket.on('message-private-debug_select_user_final', (data) => {
-    console.log(data)
+    const sel = document.getElementById("select_user");
+    for (let element of data) {
+        console.log(element)
+        const opt = document.createElement("option");
+        opt.value = element.user_name;
+        opt.text = element.user_name;
+        sel.add(opt, null)
+    }
 });
